9813197f4a6b8b79570aa742280a7e30d244a43e
Andrew Schwartz <andrew@aschwartz.me>
2019-12-04 22:14:54 -0700

    add logging

--- textRankSummarize.py
+++ textRankSummarize.py
@@ -6,25 +6,33 @@ from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from sklearn.metrics.pairwise import cosine_similarity
 import networkx as nx
+import logging
 
 import get_sections_from_textbook
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+logging.info('running...')
 
 prompt = 'impact of fugitive slaves in the civil war the life of black soldiers in the military'
 
 
 # this is using the first section (section_number) from chapter five (chapter_url)
 chapter_url = "http://www.americanyawp.com/text/05-the-american-revolution/"
-section_number = 1 # one indexed
+section_number = 2 # one indexed
 
+logging.info('getting sections from textbook')
 sections = get_sections_from_textbook.get_chapter_sections(chapter_url)
 
 sentences = sections[section_number - 1]['content']
+logging.info('using section {0} from chapter url {1}'.format(section_number, chapter_url))
 
 sentences = [sent_tokenize(s) for s in sentences]
 
 sentences = [y for x in sentences for y in x] # flatten list
 
 # Extract word vectors
+logging.info('extracting word vectors')
 word_embeddings = {}
 f = open('glove.6B.100d.txt', encoding='utf-8')
 for line in f:
@@ -33,11 +41,12 @@ for line in f:
     coefs = np.asarray(values[1:], dtype='float32')
     word_embeddings[word] = coefs
 f.close()
+logging.info('word vectors extracted')
 
 # remove punctuations, numbers and special characters
 clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
 
-#lowercase
+# lowercase
 clean_sentences = [s.lower() for s in clean_sentences]
 
 stop_words = stopwords.words('english')
@@ -48,6 +57,7 @@ def remove_stopwords(sen):
 
 clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
 
+logging.info('generating sentence vectors')
 sentence_vectors = []
 for i in clean_sentences:
     if len(i) != 0:
@@ -67,6 +77,7 @@ for i in range(len(sentences)):
 
 
 nx_graph = nx.from_numpy_array(sim_mat)
+logging.info('sorting generated sentences')
 scores = nx.pagerank(nx_graph)
 
 ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
